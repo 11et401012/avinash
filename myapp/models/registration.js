92cfd77e59698f0a60506a09cab823fae920a9bb
Avinash Patel <avinashpateljnu@gmail.com>
2018-06-28 14:48:21 +0530

    Update registration.js

--- myapp/models/registration.js
+++ myapp/models/registration.js
@@ -1,13 +1,13 @@
 
 var  mongoose=require('mongoose');
 var regdata=mongoose.Schema({
-fname:String,
-lname:String,
-email:String,
-phone:String,
-gender:String,
-skill:String,
-hobies:String
+fname:{ type:String},
+lname:{type:String},
+email:{type:String},
+phone:{type:String},
+gender:{type:String},
+skill:{type:String},
+hobies:{type:String}
 });
 var registration=mongoose.model('registration',regdata);
 module.exports = registration;
