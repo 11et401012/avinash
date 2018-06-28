9199590df0e2a9d3720acaafb17ce4c1cb30e45f
Avinash Patel <avinashpateljnu@gmail.com>
2018-06-28 14:45:09 +0530

    Update app.js

--- myapp/app.js
+++ myapp/app.js
@@ -1,68 +1,46 @@
-				var express = require('express');
-				var path = require('path');
-				var favicon = require('serve-favicon');
-				var logger = require('morgan');
-				var cookieParser = require('cookie-parser');
-				var bodyParser = require('body-parser');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var index = require('./routes/index');
+var users = require('./routes/users');
+var register=require('./routes/register');		
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://127.0.0.1/exam');
+var app = express();
 
-				var index = require('./routes/index');
-				var users = require('./routes/users');
-				var register=require('./routes/register');
-				
-				console.log("hi");
-				//var admin = require('./routes/admin');
+// view engine setup
+app.set('views', path.join(__dirname, 'public'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('*',function(req,res){
+res.render('views/index');
+});
+app.use('/register',register);
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+var err = new Error('Not Found');
+err.status = 404;
+next(err);
+});
 
-				console.log("hello");
-				var mongoose = require('mongoose');
-				mongoose.Promise = global.Promise;
-				
-           mongoose.connect('mongodb://127.0.0.1/exam');
-				//mongoose.connect('mongodb://localhost:27017/test');
-				
-				console.log('ok');
-				
-				 
-				var app = express();
+// error handler
+app.use(function(err, req, res, next) {
+// set locals, only providing error in development
+res.locals.message = err.message;
+res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-				// view engine setup
-				app.set('views', path.join(__dirname, 'public'));
-				app.engine('html', require('ejs').renderFile);
-				app.set('view engine', 'html');
+// render the error page
+res.status(err.status || 500);
+res.render('error');
+});
 
-
-				// uncomment after placing your favicon in /public
-				//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-				app.use(logger('dev'));
-				app.use(bodyParser.json());
-				app.use(bodyParser.urlencoded({ extended: false }));
-				app.use(cookieParser());
-				app.use(express.static(path.join(__dirname, 'public')));
-
-				app.get('*',function(req,res){
-					
-					res.render('views/index');
-				});
-
-				app.use('/register',register);
-				//app.use('/', index);
-				//app.use('/users', users);
-
-				// catch 404 and forward to error handler
-				app.use(function(req, res, next) {
-				  var err = new Error('Not Found');
-				  err.status = 404;
-				  next(err);
-				});
-
-				// error handler
-				app.use(function(err, req, res, next) {
-				  // set locals, only providing error in development
-				  res.locals.message = err.message;
-				  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-				  // render the error page
-				  res.status(err.status || 500);
-				  res.render('error');
-				});
-
-				module.exports = app;
+module.exports = app;
