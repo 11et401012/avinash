e3a20301c0b45659e35d00c91d9b5d65cca90ee7
Avinash Patel <avinashpateljnu@gmail.com>
2018-06-28 14:47:04 +0530

    Update registration.js

--- myapp/models/registration.js
+++ myapp/models/registration.js
@@ -1,17 +1,13 @@
 
-
 var  mongoose=require('mongoose');
 var regdata=mongoose.Schema({
-
-	fname:String,
-	lname:String,
-	email:String,
-	phone:String,
-	gender:String,
-	skill:String,
-	hobies:String
+fname:String,
+lname:String,
+email:String,
+phone:String,
+gender:String,
+skill:String,
+hobies:String
 });
-
 var registration=mongoose.model('registration',regdata);
-
 module.exports = registration;
